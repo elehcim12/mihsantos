cad332b9e53da630097452c9b1ef4d8de18a64de
elehcim12 <160048593+elehcim12@users.noreply.github.com>
2024-09-25 14:07:45 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,58 @@
 criaCartao(
-    'italiano',
-    'como falar oi em italiano?',
-    'EHI'
+    'geografia',
+    'quantos continentes existem?',
+    'sete'
 )
 
 criaCartao(
     'Geografia',
     'Qual a capital do brasil?',
-    'A capital do brasil e brasilia'
+      'A capital do brasil  é brasilia'
 )
-
 criaCartao(
-    'Pro',
-    '',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'ciencias',
+    'qual e o maoior planeta do sistema solar?',
+    'jupiter'
 )
 
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+
+riaCartao(
+    'conhecimento',
+    'quem escreveu dom casmurro?',
+    'machado de assis'
+)
+
+criaCartao(
+    'quimica',
+    'Qual e a formula quimica da agua?',
+      'H2O'
+)
+
+criaCartao(
+    'fatos',
+    'qual e o maior oceano?',
+    'oceano pacifico'
+)
+
+criaCartao(
+    'curiosidade',
+    'qual e a moeda do japao?',
+    'lene'
+)
+
+criaCartao(
+    'fatos',
+    'oque e haiku?',
+    'uma forma de poesia japonesa com dois verso'
+)
+
+criaCartao(
+    'geografia',
+    'quantos estados tem o brasil?',
+    '26 estrados e 1 destrito federal'
 )
